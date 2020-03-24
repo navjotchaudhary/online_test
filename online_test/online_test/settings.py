b64d52c726af288565d4866e821de659d0852c3f
navjotchaudhary <chaudharynavjotsingh@gmail.com>
2020-03-24 18:30:50 +0530

    created all the models but teachersDetails still remaining, created apps-student,company

--- online_test/online_test/settings.py
+++ online_test/online_test/settings.py
@@ -38,7 +38,10 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     #my defined apps 
-    'accounts'
+    'accounts',
+    'exam',
+    'student',
+    'company'
 ]
 
 MIDDLEWARE = [
